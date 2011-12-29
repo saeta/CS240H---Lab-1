68d0ee33a39ae9b0d9d5393794851c26128c8d22
saeta <saeta@cs.stanford.edu>
2011-12-29 05:32:59 -0800

    Fix the really disgusting ball of code to be much clearer.

--- src/Lab1.hs
+++ src/Lab1.hs
@@ -29,21 +29,22 @@ maxOccurences = foldl maxOcc 0
 maxOcc :: Int -> (String, Int) -> Int
 maxOcc a (_, b) = max a b
 
-calculateNumHashes :: Int -> Int -> [(String, Int)] -> [(String, Int)]
-calculateNumHashes maxL maxO' input =
-    let maxHashes = fromIntegral $ 80 - 1 - maxL
-        maxO = fromIntegral maxO'
-    in map (\(s, occ) -> (s, truncate $
-             fromIntegral occ / maxO * maxHashes)) input
+calcHashes :: RealFrac a => a -> a -> (String, Int) -> (String, Int)
+calcHashes maxOccurs maxHashes (s, occ) =
+  (s, truncate $ fromIntegral occ / maxOccurs * maxHashes)
+
+formatLine :: Int -> (String, Int) -> String
+formatLine maxTokLen (tok, numHash) =
+  take (maxTokLen + 1) (tok ++ repeat ' ') ++ replicate numHash '#' ++ "\n"
+
+addHashes :: [(String, Int)] -> [String]
+addHashes input =
+  let maxTokLen = maxLengthToken input
+      maxOccurs = fromIntegral $ maxOccurences input
+      maxHashes = fromIntegral (80 - (1 + maxTokLen))
+      hashed = map (calcHashes maxOccurs maxHashes) input
+      filtered = filter (\(_, b) -> b > 0) hashed
+  in map (formatLine maxTokLen) filtered
 
 processFile :: String -> [String]
-processFile sourceText = 
-    let processed = processInput sourceText
-        maxTokLen = maxLengthToken processed
-        intermediate = filter (\(_, b) -> b > 0) $
-          calculateNumHashes (maxLengthToken processed) 
-            (maxOccurences processed) processed
-    in map (\(tok, numHash) -> 
-               take (maxTokLen + 1) (tok ++ repeat ' ')
-                 ++ replicate numHash '#' ++ "\n")
-           intermediate
+processFile  = addHashes . processInput
