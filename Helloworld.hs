b80d45449efb1f3a2872bcfe7a243a8ee53f3dce
saeta <saeta@cs.stanford.edu>
2011-12-29 00:51:20 -0800

    Fooling around with Haskell, learning how to play with files, and work in the IO Monad.

--- Helloworld.hs
+++ Helloworld.hs
@@ -1,6 +1,25 @@
 import System
+import System.IO
+
+{-openFiles files = map (\f -> openFile f ReadMode) files
+
+--myLines openfiles = map (\f -> do { mf <- f; return readFile mf }) openFiles
 
 main = do
   f <- getArgs
-  putStrLn $ show f
-  putStrLn "Hello world!"
+  let readFiles :: [IO Handle] = openFiles f
+--  let allFiles = myLines readFiles
+--  
+--  putStrLn $ show allFiles
+  putStrLn "Hello world!" -}
+
+
+stuff :: IO [String] -> IO ()
+stuff a = do
+  foo <- a
+  if foo == []
+  then putStrLn "Empty"
+  else putStrLn "Non-empty list!"
+
+main = do
+  stuff getArgs
