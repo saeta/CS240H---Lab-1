5a79f681eba9415ac9a085247c75bfc247f04720
saeta <saeta@cs.stanford.edu>
2011-12-29 04:18:33 -0800

    Most of pure code implemented.

---
+++ src/Lab1.hs
@@ -0,0 +1,44 @@
+module Lab1(processFile) where
+import Char
+import Data.Char
+import List
+import Data.Function
+
+splitFile :: String -> [String]
+splitFile contents = map stripTailPunct $ words $ map toLower contents
+
+stripTailPunct :: String -> String
+stripTailPunct word = stp $ reverse word
+    where stp (w:ws) = if isPunctuation w then stp ws else reverse (w:ws)
+
+groupTokens :: [String] -> [(String, Int)]
+groupTokens = map (\xs -> (head xs, length xs)) . group . sort
+
+processInput :: String -> [(String, Int)]
+processInput = reverse . sortBy (compare `on` snd) . groupTokens . splitFile
+
+maxLengthToken :: [(String, Int)] -> Int
+maxLengthToken l = foldl maxLen 0 l
+
+maxLen :: Int -> (String, Int) -> Int
+maxLen a (b, _) = max a $ length b
+
+maxOccurences :: [(String, Int)] -> Int
+maxOccurences l = foldl maxOcc 0 l
+
+maxOcc :: Int -> (String, Int) -> Int
+maxOcc a (_, b) = max a b
+
+calculateNumHashes :: Int -> Int -> [(String, Int)] -> [(String, Int)]
+calculateNumHashes maxLen maxOcc' input = 
+    let maxHashes = fromIntegral $ 80 - 1 - maxLen
+        maxOcc = fromIntegral maxOcc'
+    in map (\(s, occ) -> (s, truncate $
+             (fromIntegral occ) / maxOcc * maxHashes)) input
+
+processFile :: String -> [(String, Int)]
+processFile sourceText = 
+    let processed = processInput sourceText
+    in filter (\(_, b) -> b > 0) $
+      calculateNumHashes (maxLengthToken processed) 
+        (maxOccurences processed) processed
--- src/lab1.hs
+++ src/lab1.hs
@@ -1,7 +1,11 @@
 import System
+import System.IO
+import Lab1
 
 main :: IO ()
 main = do
   f <- getArgs
   print f
+  let files = map readFile f
+  let contents = 
   putStrLn "Hello world!"
